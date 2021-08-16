c8fb68d810ceeb672827096b12240ef5e7d38ab5
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-16 16:39:17 +1200

    Initial commit.

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { getFocusedRouteNameFromRoute, NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+function Dashboard() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Nic is A BITCH</Text>
     </View>
   );
 }
 
+const nav = createNativeStackNavigator();
+const fixedMenu = createBottomTabNavigator();
+
+
+//The masthead at the top of the screen 
+function App() {
+  return (
+    <NavigationContainer>
+      {/* <nav.Navigator>
+        <nav.Screen name="Dashboard" component={Dashboard} />
+      </nav.Navigator> */}
+      <fixedMenu.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Dashboard') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <fixedMenu.Screen name = "dash" component={Dashboard}/>
+      </fixedMenu.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
