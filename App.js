a0875da3bb27d8eb1e6ecaa14ebdad46cc9c40a6
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-17 21:53:11 +1200

    Added a function for settings pages and added icon on fixed menu for settings button.

--- App.js
+++ App.js
@@ -5,11 +5,20 @@ import { getFocusedRouteNameFromRoute, NavigationContainer } from '@react-naviga
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { color } from 'react-native-reanimated';
 
 function Dashboard() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Nic is A BITCH</Text>
+      <Text>There is where the dashboard content will go</Text>
+    </View>
+  );
+}
+
+function Settings(){
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>There is where the settings content will go</Text>
     </View>
   );
 }
@@ -32,18 +41,21 @@ function App() {
 
             if (route.name === 'Dashboard') {
               iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
+                ? 'ios-home'
+                : 'md-home';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-settings' : 'md-settings';
             }
 
             // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: 'tomato',
+          tabBarActiveTintColor: '#80ccff',
           tabBarInactiveTintColor: 'gray',
         })}
       >
-        <fixedMenu.Screen name = "dash" component={Dashboard}/>
+        <fixedMenu.Screen name = "Dashboard" component={Dashboard}/>
+        <fixedMenu.Screen name = "Settings" component={Settings}/>
       </fixedMenu.Navigator>
     </NavigationContainer>
   );
