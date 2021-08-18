84b4aa1ed9beb17cf622e1e6f84c9877bbdaaf45
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-18 19:07:42 +1200

    Added camera icon for the fixed menu and page routing.

---
+++ src/screens/Photo.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const camera = function(){
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>There is where the camera content will go</Text>
+      </View>
+    );
+  }
+
+  export default camera;
