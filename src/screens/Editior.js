c82f977d87e7bbf92fefc893b68921d6d8828715
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-18 19:09:04 +1200

    Changed function to const to access it outside of the js file.

--- src/screens/Editior.js
+++ src/screens/Editior.js
@@ -2,10 +2,11 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 
-function addNote(){
+const addNote = function(){
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>There is where the notes content will go</Text>
       </View>
     );
   }
+  export default addNote;
