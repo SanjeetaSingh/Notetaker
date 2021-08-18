d694f25ab45b87091fcea534849316725c52a766
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-19 00:34:32 +1200

    Added the editior for the application using Quill.js. Still a few bugs to fix.

--- src/screens/Editior.js
+++ src/screens/Editior.js
@@ -1,12 +1,49 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import QuillEditor, { QuillToolbar } from 'react-native-cn-quill';
 
-const addNote = function(){
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>There is where the notes content will go</Text>
-      </View>
-    );
-  }
-  export default addNote;
+/**
+ * Fuction lets the user add notes to the application.
+ * 
+ * @returns the text editor that the user uses to create notes.
+ */
+const addNote = function() {
+  
+  const _editor = React.createRef();
+  return (
+    <SafeAreaView style={styles.root}>
+      <StatusBar style="auto" />
+      <QuillEditor
+          style={styles.editor}
+          ref={_editor}
+        />
+      <QuillToolbar editor={_editor} options="full" theme="light"/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    //paddingVertical: 10,
+  },
+  root: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#eaeaea',
+  },
+  editor: {
+    flex: 1,
+    padding: 0,
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginHorizontal: 30,
+    marginVertical: 5,
+    backgroundColor: 'white',
+    
+  },
+});
+  
+export default addNote;
