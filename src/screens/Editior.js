d277a16e1db66b2aab29019f0776fdee70d99565
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-19 14:32:46 +1200

    Changed the Quill js tags and refactored code based on the new imports and tags.

--- src/screens/Editior.js
+++ src/screens/Editior.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
-import QuillEditor, { QuillToolbar } from 'react-native-cn-quill';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, ScrollView } from "react-native";
+import {actions, defaultActions, RichEditor, RichToolbar,} from "react-native-pell-rich-editor";
+import HTMLView from "react-native-htmlview";
 
 /**
  * Fuction lets the user add notes to the application.
@@ -9,40 +10,77 @@ import QuillEditor, { QuillToolbar } from 'react-native-cn-quill';
  * @returns the text editor that the user uses to create notes.
  */
 const addNote = function() {
-  const _editor = React.createRef();
+  const RichText = useRef(); //reference to the RichEditor component
+  const [article, setArticle] = useState("");
+
   return (
-    <SafeAreaView style={styles.root}>
-      <StatusBar style="auto" />
-      <QuillToolbar editor={_editor} options="full" theme="light"/>
-      <QuillEditor
-          style={styles.editor}
-          ref={_editor}
-        />   
-    </SafeAreaView>
+    <ScrollView style={styles.container}>
+      <RichToolbar
+        style={[styles.richBar]}
+        editor={RichText}
+        disabled={false}
+        iconTint={"#000"}
+        selectedIconTint={"#80ccff"}
+        disabledIconTint={"#80ccff"}
+        iconSize={20}
+        actions={[
+          ...defaultActions,
+          actions.insertOrderedList,
+          actions.insertImage,
+          actions.setStrikethrough,
+          actions.heading1,
+          actions.heading2,  
+        ]}
+        // map icons for self made actions
+        iconMap={{
+          [actions.heading1]: ({ tintColor }) => (
+            <Text style={[styles.tib, { color: tintColor }]}>H1</Text>
+          ),
+          [actions.heading2]: ({ tintColor }) => (
+            <Text style={[styles.tib, { color: tintColor }]}>H2</Text>
+          ),
+        }}
+      />
+      <RichEditor
+        disabled={false}
+        containerStyle={styles.editor}
+        ref={RichText}
+        style={styles.rich}
+        placeholder={"Start Writing Here"}
+        onChange={(text) => setArticle(text)}
+      />
+      <HTMLView value={article} stylesheet={styles} />
+    </ScrollView>
   );
-  
-}
+};
+
+export default addNote;
 
 const styles = StyleSheet.create({
-  title: {
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    //paddingVertical: 10,
-  },
-  root: {
+  container: {
     flex: 1,
-    marginBottom: StatusBar.currentHeight,
-    backgroundColor: '#eaeaea',
+    marginTop: 0,
+    backgroundColor: "#F5FCFF",
   },
   editor: {
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  rich: {
+    marginTop:5,
+    minHeight: 650,
     flex: 1,
-    padding: 1,
-    borderColor: 'gray',
-    borderWidth: 2,
-    marginHorizontal: 25,
-    marginVertical: 10,
-    backgroundColor: 'white',
+    marginHorizontal: 30,
+    marginVertical: 5,
+  },
+  richBar: {
+    height: 50,
+    backgroundColor: "#F5FCFF",
+  },
+  tib: {
+    textAlign: "center",
+    color: "#515156",
   },
 });
-  
-export default addNote;
+
