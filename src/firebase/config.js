78eb5aaa5350662b39d6f67dd22410fe8299fbef
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-24 19:38:59 +1200

    Added firebase configuration for the sign up and login page.

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBBHzPsXDrmnaBa4xz2jTqrg-BfbFdUAK4',
+    authDomain: 'notetakingapplication-7c312',
+    databaseURL: 'https://notetakingapplication-7c312-default-rtdb.firebaseio.com/',
+    projectId: 'notetakingapplication-7c312',
+    storageBucket: 'notetakingapplication-7c312.appspot.com',
+    messagingSenderId: '317956323910',
+    appId: '1:317956323910:ios:abc6d555030202273fe847',
+  };
+  
+  if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+  }
+  
+  export { firebase };
