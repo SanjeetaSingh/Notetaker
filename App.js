9e6ed1d9e58e10c7ba00dae77261279f61715bf0
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-24 19:37:43 +1200

    Added stack navigation to start with logging in at the start and going into the application after

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LoginScreen from '../notetaking-app/src/screens/login/LoginScreen' 
+import RegisterScreen from '../notetaking-app/src/screens/login/RegisterScreen'
+import Menu from './src/navigation/BottomMenu';
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
-import Menu from "./src/navigation/BottomMenu"
+const Stack = createStackNavigator();
 
-export default function App(){
-  return (
-    Menu()
-  );
-}
+export default function App() {  
+    return (	
+      <NavigationContainer>
+       <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Registration" component={RegisterScreen} />
+          <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    )	;
+  }
+
+
+
+
