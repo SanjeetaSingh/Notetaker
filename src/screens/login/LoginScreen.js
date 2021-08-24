26915185115ac3762a64b772c60cdd3c5eb29b55
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-24 21:20:44 +1200

    Added styling for the login screen.

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -45,7 +45,7 @@ export default function LoginScreen({navigation}) {
                 keyboardShouldPersistTaps="always">
                 <Image
                     style={styles.logo}
-                    source={require('../../../assets/icon.png')}
+                    source={require('../../../assets/logos.png')}
                 />
                 <TextInput
                     style={styles.input}
@@ -90,23 +90,23 @@ const styles = StyleSheet.create({
     logo: {
         flex: 1,
         height: 120,
-        width: 90,
+        width: 250,
         alignSelf: "center",
         margin: 30
     },
     input: {
-        height: 48,
+        height: 50,
         borderRadius: 5,
         overflow: 'hidden',
         backgroundColor: 'white',
-        marginTop: 10,
+        marginTop: 5,
         marginBottom: 10,
         marginLeft: 30,
         marginRight: 30,
         paddingLeft: 16
     },
     button: {
-        backgroundColor: '#788eec',
+        backgroundColor: '#9AC4F8',
         marginLeft: 30,
         marginRight: 30,
         marginTop: 20,
@@ -117,20 +117,20 @@ const styles = StyleSheet.create({
     },
     buttonTitle: {
         color: 'white',
-        fontSize: 16,
+        fontSize: 20,
         fontWeight: "bold"
     },
     footerView: {
         flex: 1,
         alignItems: "center",
-        marginTop: 20
+        marginTop: 30
     },
     footerText: {
         fontSize: 16,
         color: '#2e2e2d'
     },
     footerLink: {
-        color: "#788eec",
+        color: "#9AC4F8",
         fontWeight: "bold",
         fontSize: 16
     }
