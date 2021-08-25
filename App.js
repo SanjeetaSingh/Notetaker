17e225617c43b5ae730a92e0248df81243615fa9
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-25 17:05:07 +1200

    Added javadoc and comments to class.

--- App.js
+++ App.js
@@ -11,11 +11,19 @@ if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
 
+/**
+ * This function calls on the functions
+ * in other classes to let the user view the
+ * application on the screen. 
+ * 
+ * @returns The UI of the application.
+ */
 export default function App() {  
-    return (	
+    return (
+      //The order of the screen will displayed to the user	
       <NavigationContainer>
        <Stack.Navigator screenOptions={screenOptionStyle}>
-          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Login" component={LoginScreen}  />
           <Stack.Screen name="Registration" component={RegisterScreen} />
           <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }}/>
       </Stack.Navigator>
@@ -23,6 +31,9 @@ export default function App() {
     )	;
   }
 
+  /**
+   * The Styling of the stack navigation screens
+   */
   const screenOptionStyle = {
     headerStyle: {
       backgroundColor: "#9AC4F8",
