8a5a599b0dcc368678e0b1218ef1f2be075d2fd4
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-27 18:29:44 +1200

    Added functions to class to show the camera on screen and add icon to cancel preview.

--- src/screens/main/Photo.js
+++ src/screens/main/Photo.js
@@ -1,20 +1,74 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { View, Text} from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+import { Camera } from 'expo-camera';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
 
-/**
- * Function creates the add photo screen that 
- * uses the camera component to let use to take photos
- * and save them. 
- * 
- * @returns 
- */
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const CAPTURE_SIZE = Math.floor(WINDOW_HEIGHT * 0.08);
+
 const camera = function(){
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>There is where the camera content will go</Text>
-      </View>
-    );
+
+  const cameraRef = useRef();
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [isPreview, setIsPreview] = useState(false);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+
+  useEffect(() => {
+    onHandlePermission();
+  }, []);
+
+  const onHandlePermission = async () => {
+    const { status } = await Camera.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
+  };
+
+  const onCameraReady = () => {
+    setIsCameraReady(true);
+  };
+
+  const cancelPreview = async () => {
+    await cameraRef.current.resumePreview();
+    setIsPreview(false);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text style={styles.text}>No access to camera</Text>;
   }
 
-  export default camera;
+  return (
+    <View style={styles.container}>
+      <Camera
+        ref={cameraRef}
+        style={styles.container}
+        type={cameraType}
+        onCameraReady={onCameraReady}
+        useCamera2Api={true}
+      />
+      <View style={styles.container}>
+        {isPreview && (
+          <TouchableOpacity
+            onPress={cancelPreview}
+            style={styles.closeButton}
+            activeOpacity={0.7}
+          >
+            <AntDesign name='close' size={32} color='#fff' />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  
+});
+export default camera;
