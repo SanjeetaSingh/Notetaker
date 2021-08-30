a879ecf0d0e704593b7fa711bea0440a5205f737
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-30 21:44:29 +1200

    Added save button to save the text to firebase. Fixed to be made.

--- src/screens/main/Editor.js
+++ src/screens/main/Editor.js
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-import React, { useRef, useState } from "react";
-import { StyleSheet, Text, ScrollView } from "react-native";
+import React, { useEffect,useRef, useState } from "react";
+import { StyleSheet, Text, ScrollView , Keyboard, TouchableOpacity, View } from "react-native";
 import {actions, defaultActions, RichEditor, RichToolbar,} from "react-native-pell-rich-editor";
+import { firebase } from '../../firebase/config'
 
 /**
  * Function created the editor screen which lets the
@@ -11,10 +12,56 @@ import {actions, defaultActions, RichEditor, RichToolbar,} from "react-native-pe
  * 
  * @returns The text editor screen.
  */
-const addNote = function() {
+ export default function addNote(props) {
   const RichText = useRef(); //reference to the RichEditor component
-  const [setArticle] = useState("");
+  //const [setArticle] = useState("");
 
+  const [entityText, setEntityText] = useState('')
+  const [entities, setEntities] = useState([])
+
+    const entityRef = firebase.firestore().collection('entities')
+    const userID = props.extraData.id
+
+    useEffect(() => {
+        entityRef
+            .where("authorID", "==", userID)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(
+                querySnapshot => {
+                    const newEntities = []
+                    querySnapshot.forEach(doc => {
+                        const entity = doc.data()
+                        entity.id = doc.id
+                        newEntities.push(entity)
+                    });
+                    setEntities(newEntities)
+                },
+                error => {
+                    console.log(error)
+                }
+            )
+    }, [])
+
+    const onAddButtonPress = () => {
+        if (entityText && entityText.length > 0) {
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+            const data = {
+                text: entityText,
+                authorID: userID,
+                createdAt: timestamp,
+            };
+            entityRef
+                .add(data)
+                .then(_doc => {
+                    setEntityText('')
+                    Keyboard.dismiss()
+                })
+                .catch((error) => {
+                    alert(error)
+                });
+        }
+    }
+  
   /**
    * Add images from the gallery
    */
@@ -53,7 +100,9 @@ const addNote = function() {
           actions.insertImage,
           actions.setStrikethrough,
           actions.heading1,
-          actions.heading2,  
+          actions.heading2,
+          actions.undo, 
+          actions.redo, 
         ]}
         // Creating icons for actions on toolbar
         iconMap={{
@@ -73,8 +122,16 @@ const addNote = function() {
         ref={RichText}
         style={styles.rich}
         placeholder={"Start Writing Here"}
-        onChange={(text) => setArticle(text)}
+        onChange={(text) => setEntityText(text)}
+        value={entityText}
       />
+      <View style={styles.buttonContainer}>
+        <View style={styles.formContainer}>
+          <TouchableOpacity style={styles.button} onPress={onAddButtonPress}>
+            <Text style={styles.buttonText}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ScrollView>
   );
 };
@@ -96,7 +153,7 @@ const styles = StyleSheet.create({
   },
   rich: {
     marginTop:5,
-    minHeight: 650,
+    minHeight: 600,
     flex: 1,
     marginHorizontal: 30,
     marginVertical: 5,
@@ -113,7 +170,29 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#515156",
   },
-
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center'
+},
+formContainer: {
+  flexDirection: 'row',
+  height: 80,
+  marginBottom: 20,
+  flex: 1,
+  paddingTop: 10,
+  paddingBottom: 10,
+  paddingLeft: 30,
+  paddingRight: 30,
+  justifyContent: 'center',
+  alignItems: 'center'
+},
+input: {
+  height: 48,
+  borderRadius: 5,
+  overflow: 'hidden',
+  backgroundColor: 'white',
+  paddingLeft: 16,
+  flex: 1,
+  marginRight: 5
+},
 });
-
-export default addNote;
