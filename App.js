4269dec1b0a1d0c3abc05b2580756945f7bd68b1
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-08-30 18:49:35 +1200

    Added a persistent login feature so user gets logged in once opening the application.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import LoginScreen from '../notetaking-app/src/screens/login/LoginScreen' 
 import RegisterScreen from '../notetaking-app/src/screens/login/RegisterScreen'
 import Screens from './src/navigation/BottomMenu';
 import {decode, encode} from 'base-64'
+import { firebase } from './src/firebase/config'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
@@ -18,14 +19,45 @@ const Stack = createStackNavigator();
  * 
  * @returns The UI of the application.
  */
-export default function App() {  
+export default function App() { 
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []); 
+
     return (
       //The order of the screen will displayed to the user	
       <NavigationContainer>
-       <Stack.Navigator screenOptions={screenOptionStyle}>
-          <Stack.Screen name="Login" component={LoginScreen}  />
-          <Stack.Screen name="Registration" component={RegisterScreen} />
-          <Stack.Screen name="Screen" component={Screens} options={{ headerShown: false } }/>
+       <Stack.Navigator>
+        { user ? (
+          <Stack.Screen name="Screens" options={{ headerShown: false } }>
+            {props => (<Screens {...props} extraData={user} />)}
+          </Stack.Screen>
+        ) : (
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegisterScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
     )	;
