0040d3740b9dc208f5c359d7b67b72e8d0b6cf33
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-09-05 23:24:54 +1200

    Clean up of code and added 2 more screens to navigate.

--- src/navigation/Navigate.js
+++ src/navigation/Navigate.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer,  DefaultTheme, DarkTheme } from '@react-navigation/native'
-import {Provider as PaperProvider, DefaultTheme as PaperDefaultTheme, DarkTheme as PaperDarkTheme, } from 'react-native-paper';
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native'
+import { Provider as PaperProvider, DefaultTheme as PaperDefaultTheme, DarkTheme as PaperDarkTheme, } from 'react-native-paper';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createStackNavigator } from "@react-navigation/stack";
@@ -9,20 +9,22 @@ import { useSelector } from "react-redux";
 import firebase from 'firebase'
 
 //Imports to other files
-import {Button} from 'react-native';
+import { Button } from 'react-native';
 import Dashboard from '../screens/main/Dashboard'
 import camera from '../screens/main/Photo';
 import AddNote from '../screens/main/Editor';
 import Settings from '../screens/main/Settings';
 import LoginScreen from '../screens/login/LoginScreen';
 import RegistrationScreen from '../screens/login/RegisterScreen';
-
+import About from '../screens/main/About'
+import Update from '../screens/main/Update'
+import DailyToDo from '../screens/main/DailyToDo';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 /**
- * This function creates the Dashboard screen header.
+ * This function creates a header for the dashboard screen.
  * 
  * @returns The screen header.
  */
@@ -30,61 +32,78 @@ const MainNavigator = (nav) => {
   return (
     <Stack.Navigator screenOptions={screenOptionStyle}>
       <Stack.Screen name="Dashboard" >
-      {props => (<Dashboard{...props} extraData={nav.extraData}/>)}
+        {props => (<Dashboard{...props} extraData={nav.extraData} />)}
       </Stack.Screen>
     </Stack.Navigator>
   );
 }
 
 /**
- * This function creates the Add a photo screen header.
+ * This function creates a header for the camera screen.
  * 
  * @returns The screen header.
  */
- const PhotoNavigator = ({navigation}) => {
+const PhotoNavigator = ({ navigation }) => {
   return (
     <Stack.Navigator screenOptions={screenOptionStyle}>
       <Stack.Screen name="Add Photo" component={camera} options={{
-          headerLeft: () => (
-            //Creating a back button to go back to previous page
-              <Button
-                  title="Back"
-                  onPress={() => {
-                  navigation.goBack();
-                  }}
-              />
-          ),
-      }}/>
+        headerLeft: () => (
+          //Creating a back button to go back to previous page
+          <Button
+            title="Back"
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+        ),
+      }} />
     </Stack.Navigator>
   );
 }
 
 /**
- * This function creates the Settings screen header.
+ * This function creates a header for the settings screen.
  * 
  * @returns The screen header.
  */
- const SettingsNavigator = (nav) => {
+const SettingsNavigator = (nav) => {
   return (
     <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen name="Settings"> 
-          {props => (<Settings{...props} extraData={nav.extraData}/>)}
+      <Stack.Screen name="Settings">
+        {props => (<Settings{...props} extraData={nav.extraData} />)}
+      </Stack.Screen>
+      <Stack.Screen name="About" component={About}></Stack.Screen>
+      <Stack.Screen name="Update" component={Update}></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
+
+/**
+ * This function creates a header for editor screen.
+ * 
+ * @returns The screen header.
+ */
+const NotesNavigator = (nav) => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Editor">
+        {props => (<AddNote{...props} extraData={nav.extraData} />)}
       </Stack.Screen>
     </Stack.Navigator>
   );
 }
 
 /**
- * This function creates the Add a note screen header.
+ * This function creates a header for daily to do screen.
  * 
  * @returns The screen header.
  */
- const NotesNavigator = (nav) => {
+const ToDoNavigator = (nav) => {
   return (
     <Stack.Navigator screenOptions={screenOptionStyle}>
-      <Stack.Screen name="Editor">
-      {props => (<AddNote{...props} extraData={nav.extraData}/>)}
-      </Stack.Screen>     
+      <Stack.Screen name="Daily To-Do">
+        {props => (<DailyToDo{...props} extraData={nav.extraData} />)}
+      </Stack.Screen>
     </Stack.Navigator>
   );
 }
@@ -97,59 +116,75 @@ const MainNavigator = (nav) => {
  * 
  * @returns The fixed menu on the application screens.
  */
- const Menu = (nav) =>  {
+const Menu = (nav) => {
   return (
-    //The headers for the tab navigation is set to not show
-      <Tab.Navigator screenOptions={{ headerShown: false }}> 
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      {/* The Dashboard tab navigation */}
       <Tab.Screen name='Dash' options={{
-            tabBarLabel:'Dashboard',
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="ios-home" color={color} size={26} />
-            ),
-            tabBarActiveTintColor: '#9AC4F8',
-            tabBarInactiveTintColor: 'gray',
+        tabBarLabel: 'Dashboard',
+        tabBarIcon: ({ color }) => (
+          <Ionicons name="ios-home" color={color} size={26} />
+        ),
+        tabBarActiveTintColor: '#9AC4F8',
+        tabBarInactiveTintColor: 'gray',
       }}>
-        {props => (<MainNavigator {...props} extraData={nav.extraData}/>)}
+        {props => (<MainNavigator {...props} extraData={nav.extraData} />)}
       </Tab.Screen>
+
+      {/* The Camera tab navigation */}
       <Tab.Screen
-      //The add a photo tab navigation with a camera icon
-            name="Photo"
-            component={PhotoNavigator}
-            options={{
-              tabBarLabel:'Camera',
-              tabBarIcon: ({ color }) => (
-                <Ionicons name="ios-camera" color={color} size={26} />
-              ),
-              tabBarActiveTintColor: '#9AC4F8',
-              tabBarInactiveTintColor: 'gray',
-            }}
+        name="Photo"
+        component={PhotoNavigator}
+        options={{
+          tabBarLabel: 'Camera',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-camera" color={color} size={26} />
+          ),
+          tabBarActiveTintColor: '#9AC4F8',
+          tabBarInactiveTintColor: 'gray',
+        }}
       />
+
+      {/* The Daily todo tab navigation */}
       <Tab.Screen
-      //The settings tab navigation with a gear icon
-            name="setting" options={{
-              tabBarLabel:'Setting',
-              tabBarIcon: ({ color }) => (
-                <Ionicons name="ios-settings" color={color} size={26} />
-              ),
-              tabBarActiveTintColor: '#9AC4F8',
-              tabBarInactiveTintColor: 'gray',
-            }}>
-            {props => (<SettingsNavigator {...props} extraData={nav.extraData}/>)}
-      </Tab.Screen>
-      
-      <Tab.Screen 
-      //The add a note tab navigation with a plus icon
-            name="Edit" options={{
-            tabBarLabel:'Add Notes',
-            tabBarIcon: ({ color }) => (
+        name="To-Do"
+        component={ToDoNavigator}
+        options={{
+          tabBarLabel: 'To-Do',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="document" color={color} size={26} />
+          ),
+          tabBarActiveTintColor: '#9AC4F8',
+          tabBarInactiveTintColor: 'gray',
+        }}
+      />
+
+      {/* The Editor tab navigation */}
+      <Tab.Screen
+        name="Edit" options={{
+          tabBarLabel: 'Add Notes',
+          tabBarIcon: ({ color }) => (
             <Ionicons name="ios-add-circle" color={color} size={26} />
-            ),
-            tabBarActiveTintColor: '#9AC4F8',
-            tabBarInactiveTintColor: 'gray',
-          }}>
-         {props => (<NotesNavigator {...props} extraData={nav.extraData}/>)}
+          ),
+          tabBarActiveTintColor: '#9AC4F8',
+          tabBarInactiveTintColor: 'gray',
+        }}>
+        {props => (<NotesNavigator {...props} extraData={nav.extraData} />)}
       </Tab.Screen>
-      </Tab.Navigator>
+
+      {/* The Settings tab navigation */}
+      <Tab.Screen
+        name="setting" options={{
+          tabBarLabel: 'Setting',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="ios-settings" color={color} size={26} />
+          ),
+          tabBarActiveTintColor: '#9AC4F8',
+          tabBarInactiveTintColor: 'gray',
+        }}>
+        {props => (<SettingsNavigator {...props} extraData={nav.extraData} />)}
+      </Tab.Screen>
+    </Tab.Navigator>
   );
 }
 
@@ -157,57 +192,55 @@ const LoginRegisterStack = createStackNavigator();
 
 export default () => {
   const themeReducer = useSelector(({ themeReducer }) => themeReducer);
-  const Tab = createBottomTabNavigator(); 
-
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
 
 
   useEffect(() => {
-      const usersRef = firebase.firestore().collection('users');
-      firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-              usersRef
-                  .doc(user.uid)
-                  .get()
-                  .then((document) => {
-                      const userData = document.data()
-                      setLoading(false)
-                      setUser(userData)
-                  })
-                  .catch((error) => {
-                      setLoading(false)
-                  });
-          } else {
-              setLoading(false)
-          }
-      });
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
   }, []);
 
   return (
-      <NavigationContainer theme={themeReducer.theme ? DarkTheme : DefaultTheme}>
-        <PaperProvider theme={themeReducer.theme ? PaperDarkTheme : PaperDefaultTheme}>
-          <LoginRegisterStack.Navigator screenOptions={screenOptionStyle}>
-              {user ? (
-                  <LoginRegisterStack.Screen name="Menu" options={{ headerShown: false }}>
-                      {props => (<Menu{...props} extraData={user} />)}
-                  </LoginRegisterStack.Screen>
-              ) : (
-                  <>
-                    <LoginRegisterStack.Screen name="Login" component={LoginScreen} />
-                    <LoginRegisterStack.Screen name="Registration" component={RegistrationScreen} />
-                  </>
-              )}
-          </LoginRegisterStack.Navigator>
-          </PaperProvider>
-      </NavigationContainer>
+    <NavigationContainer theme={themeReducer.theme ? DarkTheme : DefaultTheme}>
+      <PaperProvider theme={themeReducer.theme ? PaperDarkTheme : PaperDefaultTheme}>
+        <LoginRegisterStack.Navigator screenOptions={screenOptionStyle}>
+          {user ? (
+            <LoginRegisterStack.Screen name="Menu" options={{ headerShown: false }}>
+              {props => (<Menu{...props} extraData={user} />)}
+            </LoginRegisterStack.Screen>
+          ) : (
+            <>
+              <LoginRegisterStack.Screen name="Login" component={LoginScreen} />
+              <LoginRegisterStack.Screen name="Registration" component={RegistrationScreen} />
+            </>
+          )}
+        </LoginRegisterStack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
   );
 };
 
 /**
  * Screen header styling.
  */
- const screenOptionStyle = {
+const screenOptionStyle = {
   headerStyle: {
     backgroundColor: "#9AC4F8",
   },
