821deb623dbd3c53e44814d9badeaa1f9340b6a7
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-09-15 12:49:34 +1200

    Added a new screen to application.

---
+++ src/screens/main/Search.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+function Search(navigation) {
+    return (
+      <View>
+         <Text>search</Text>
+      </View>
+    );
+  }
+
+  export default Search;
