f1e267e574f05cd0c307a7ed9a625d210bea5c8e
Sanjeeta Singh <singhsanj2@ecs.vuw.ac.nz>
2021-09-24 17:45:40 +1200

    Add javadoc to code.

--- src/theme/redux/reducers/theme.reducers.js
+++ src/theme/redux/reducers/theme.reducers.js
@@ -1,8 +1,13 @@
 import { DARK_THEME,LIGHT_THEME } from "../constants";
+
+//Setting the initial state to false
 const initialState = {
     theme: false
 };
 
+/**
+ * Switch cases to switch between themes
+ */
 export default (theme = initialState,{ type }) => {
     switch (type) {
         case DARK_THEME:
