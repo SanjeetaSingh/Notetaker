0a89fd7c5c73365f788f76dd4af76c73a3c59761
Sanjeeta Singh <Sanj33ta@gmail.com>
2022-01-26 18:40:10 +1300

    Added .env file to hide the firebase variables.

---
+++ src/back-end/firebase/config.js
@@ -0,0 +1,26 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+import config from '../../env'
+
+/**
+ * The firebase configuration. Information is 
+ * according to the project created in firebase. 
+ */
+
+
+export const firebaseConfig = {
+    apiKey: config.API_KEY,
+    authDomain: config.AUTH_DOMAIN,
+    databaseURL: config.DATABASE_URL,
+    projectId: config.PROJECT_ID,
+    storageBucket: config.STORAGE_BUCKET,
+    messagingSenderId: config.MESSAGESENDER_ID,
+    appId: config.APP_ID,
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
